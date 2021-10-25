89080740cb7a103dc7c78462803040f00ff8c19a
Android1337 <51016908+Android1337@users.noreply.github.com>
2021-10-25 19:16:59 +0200

    Updated

--- 1337/no_define.h
+++ 1337/no_define.h
@@ -43,7 +43,7 @@ namespace signatures {
 }
 
 namespace offsets {
-	uintptr_t OwningGameInstance = 0x180;
+	uintptr_t OwningGameInstance = 0x188;
 	uintptr_t LocalPlayers = 0x38;
 	uintptr_t PlayerController = 0x30;
 	uintptr_t PlayerCameraManager = 0x2B8;
@@ -59,16 +59,16 @@ namespace offsets {
 	uintptr_t FireStartLoc = 0x880;
 	uintptr_t RelativeLocation = 0x11c;
 	uintptr_t RelativeRotation = 0x128;
-	uintptr_t CurrentWeapon = 0x600;
+	uintptr_t CurrentWeapon = 0x610;
 	uintptr_t PlayerState = 0x240;
 	uintptr_t Mesh = 0x280;
-	uintptr_t TeamIndex = 0xED0;
+	uintptr_t TeamIndex = 0xED8;
 
 
-	//Exploits
+	//Exploits I cba updating those
 	uintptr_t bDisableEquipAnimation = 0x2B3;
-	uintptr_t bADSWhileNotOnGround = 0x3b21; // 0x3b11;
-	uintptr_t ReviveFromDBNOTime = 0x3430; // 0x3420;
+	uintptr_t bADSWhileNotOnGround = 0x3b21;
+	uintptr_t ReviveFromDBNOTime = 0x3430;
 	uintptr_t CustomTimeDilation = 0x98;
 	uintptr_t LastFireTime = 0x9BC;
 	uintptr_t LastFireTimeVerified = 0x9C0;
